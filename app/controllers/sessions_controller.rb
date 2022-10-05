6f69ab66aa0c689c00cc42c80c5e9c57f3de5345
gatwi <faith.murithi@student.moringaschool.com>
2022-10-05 09:19:26 +0300

    created users controller and updated serializers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
-class SessionsController < ApplicationController
+class SessionsController < ApplicationController 
+
+    def create 
+        user = User.find_bu(username: params[:username])
+        session[:user_id] = user.user_id
+        render json: user
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
 end
